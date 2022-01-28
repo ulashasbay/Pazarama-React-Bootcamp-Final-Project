f1c349eb48185992144dd9fe276d96b7fdeab2ae
Ulaş Hasbay <ulashasbay@gmail.com>
2022-01-28 18:40:01 +0300

    yup bug fix

--- src/schema/AdminLoginVal.js
+++ src/schema/AdminLoginVal.js
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 const AdminLoginVal = yup.object().shape({
   username: yup.string().required("Lütfen Kullanıcı Adınızı Giriniz!")
-  .matches(/^kodluyoruz$/, " Kullanıcı Adı Hatalı!"),
+  .matches(/^kodluyoruz$/, "Kullanıcı Adı Hatalı!"),
   password: yup
     .string()
     .required("Lütfen Şifrenizi Giriniz!")
